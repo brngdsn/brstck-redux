e5b3807f3f1bf91132acd2435d41f35a5b5c681f
bragoods <bragoods@>
2016-05-09 12:03:05 -0400

    added md2 toolbar

--- src/app/brstck.component.ts
+++ src/app/brstck.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import {MdToolbar} from '@angular2-material/toolbar';
 
 @Component({
   moduleId: module.id,
   selector: 'brstck-app',
   templateUrl: 'brstck.component.html',
-  styleUrls: ['brstck.component.css']
+  styleUrls: ['brstck.component.css'],
+  directives: [
+    MdToolbar
+  ]
 })
 export class BrstckAppComponent {
   title = 'brstck works!';
