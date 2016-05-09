049dd82e4b6ea6375db825fcc2f8c3bad0663813
bragoods <bragoods@>
2016-05-09 13:43:38 -0400

    added md2 button and icon modules, added md2 fonts

--- src/app/brstck.component.ts
+++ src/app/brstck.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {MdToolbar} from '@angular2-material/toolbar';
 import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
+import {MdButton} from '@angular2-material/button';
+import {MdIcon, MdIconRegistry} from '@angular2-material/icon';
 
 @Component({
   moduleId: module.id,
@@ -9,7 +11,12 @@ import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
   styleUrls: ['brstck.component.css'],
   directives: [
     MdToolbar,
-    MD_SIDENAV_DIRECTIVES
+    MD_SIDENAV_DIRECTIVES,
+    MdButton,
+    MdIcon
+  ],
+  providers: [
+    MdIconRegistry
   ]
 })
 export class BrstckAppComponent {
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
+import {HTTP_PROVIDERS} from '@angular/http';
 import { BrstckAppComponent, environment } from './app/';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(BrstckAppComponent);
+bootstrap(BrstckAppComponent, [HTTP_PROVIDERS]);
